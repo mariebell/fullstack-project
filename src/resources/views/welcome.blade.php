b3471594b07959db991177f2a39f025a6ca6bf7a
mariebell <sugary.macarons@gmail.com>
2022-06-20 19:07:46 +0900

    Update welcome.blade.php

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -2,8 +2,6 @@
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fullstack Project</title>
     <link rel="stylesheet" href="/css/app.css" />
 </head>
