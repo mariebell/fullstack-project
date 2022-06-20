81360ba5e97c074b921b5083d15f28b77500b9ce
mariebell <sugary.macarons@gmail.com>
2022-06-20 19:30:13 +0900

    Update welcome.blade.php

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Fullstack Project</title>
-    <link rel="stylesheet" href="/css/app.css" />
+    <link rel="stylesheet" href="{{ mix('/css/app.css') }}" />
 </head>
 <body>
     <div id="example"></div>
